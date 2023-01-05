09a910db99a5a7638bae4b14ec07d3dbe8785023
Alex Schaffer <alexschaffer@gmail.com>
2023-01-05 14:53:59 -0500

    fixed catch block

--- catch_block_assignment1/catch_block_assignment1/Program.cs
+++ catch_block_assignment1/catch_block_assignment1/Program.cs
@@ -9,10 +9,8 @@ class Program
             Console.WriteLine("Pick a number!");
             int number1 = Convert.ToInt32(Console.ReadLine()); // instead of creating list of intergers i did toInt32 - is that okay?
             Console.WriteLine("Pick another number!");
-            int number2 = Convert.ToInt32(Console.ReadLine()); // second number creation
-            Console.WriteLine("Let's divid the 2:"); 
-            int number3 = number1 / number2;
-            Console.WriteLine(number1 + " divided by " + number2 + " equals " + number3); //the line that does the divison and spills ito ut
+            int number2 = number1 / 2;
+            Console.WriteLine(number1 + " divided by 2 equals" + number2); //the line that does the divison and spills ito ut
         }
         catch (FormatException)
         {
